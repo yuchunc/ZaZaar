36796d22a440de2bad38085d0a8d3b6db0cf55ca
Mickey <poverwhelming@gmail.com>
2018-12-03 17:55:44 +0800

    setuped bulma

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const glob = require('glob');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
@@ -13,7 +12,9 @@ module.exports = (env, options) => ({
     ]
   },
   entry: {
-      './js/app.js': ['./js/app.js'].concat(glob.sync('./vendor/**/*.js'))
+    app: [
+      './js/app.js',
+    ]
   },
   output: {
     filename: 'app.js',
@@ -28,14 +29,40 @@ module.exports = (env, options) => ({
           loader: 'babel-loader'
         }
       },
+            {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader",
+        query: {
+          name: "fonts/[hash].[ext]",
+          mimetype: "application/font-woff"
+        }
+      },
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        test: /\.(eot|svg|ttf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader",
+        query: {
+          name: "fonts/[hash].[ext]"
+        }
+      },
+      {
+        test: /\.(svg|jpg|png|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]?[hash]',
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(sc|sa|c)ss$/,
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
       }
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: '../css/app.css' }),
+    new MiniCssExtractPlugin({ filename: '../css/app.css', allChunks: true }),
     new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
   ]
 });
--- config/dev.exs
+++ config/dev.exs
@@ -12,11 +12,9 @@ config :zazaar, ZaZaarWeb.Endpoint,
   code_reloader: true,
   check_origin: false,
   watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
+    yarn: [
+      "run",
+      "watch",
       cd: Path.expand("../assets", __DIR__)
     ]
   ]
