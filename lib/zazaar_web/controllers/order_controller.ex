86fda36a3c4c1675493bfb0dba4ec64afebaefa7
Mickey <poverwhelming@gmail.com>
2018-12-06 10:50:21 +0800

    setup order index and show

--- lib/zazaar_web/controllers/order_controller.ex
+++ lib/zazaar_web/controllers/order_controller.ex
@@ -4,4 +4,8 @@ defmodule ZaZaarWeb.OrderController do
   def index(conn, _) do
     render(conn, "index.html")
   end
+
+  def show(conn, %{"id" => id}) do
+    render(conn, "show.html", id: id)
+  end
 end
