8176b0c32bb9bf096452d77d836d49d8cdf44af8
Mickey <poverwhelming@gmail.com>
2018-12-11 18:19:00 +0800

    added session_controller with oatuh callback

---
+++ lib/zazaar_web/controllers/session_controller.ex
@@ -0,0 +1,27 @@
+defmodule ZaZaarWeb.SessionController do
+  use ZaZaarWeb, :controller
+
+  alias ZaZaar.Auth.Guardian.Plug, as: GPlug
+
+  def delete(conn, _params) do
+    conn
+    |> GPlug.sign_out()
+    |> put_flash(:info, dgettext("success", "Logout Successful"))
+    |> redirect(to: "/")
+  end
+
+  def create(%{assigns: %{ueberauth_failure: _fails}} = conn, _params) do
+    conn
+    |> put_flash(:error, dgettext("error", "Login Failed"))
+    |> redirect(to: "/")
+  end
+
+  def create(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    {:ok, user} = Account.fb_auth(auth.uid, auth.info)
+
+    conn
+    |> GPlug.sign_in(user)
+    |> put_flash(:success, dgettext("success", "Login Successful"))
+    |> redirect(to: "/")
+  end
+end
--- lib/zazaar_web/router.ex
+++ lib/zazaar_web/router.ex
@@ -7,6 +7,18 @@ defmodule ZaZaarWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+
+    plug(
+      Guardian.Plug.Pipeline,
+      module: ZaZaar.Auth.Guardian
+    )
+
+    plug(Guardian.Plug.VerifySession)
+    plug(Guardian.Plug.LoadResource, allow_blank: true)
+  end
+
+  pipeline :auth do
+    plug(Guardian.Plug.EnsureAuthenticated)
   end
 
   pipeline :api do
@@ -21,7 +33,10 @@ defmodule ZaZaarWeb.Router do
     get "/privacy", PageController, :privacy
     get "/service", PageController, :service
 
-    # TODO introduce login path
+    scope "/auth" do
+      get("/:provider", SessionController, :request)
+      get("/:provider/callback", SessionController, :create)
+    end
 
     # TODO pipe this in to auth
     scope "/" do
