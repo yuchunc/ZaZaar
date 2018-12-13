f6aeb4e528444555036515c9fd58651e1ab62a89
Mickey <poverwhelming@gmail.com>
2018-12-13 17:12:49 +0800

    stores user access_token

--- lib/zazaar/auth/auth.ex
+++ lib/zazaar/auth/auth.ex
@@ -3,16 +3,25 @@ defmodule ZaZaar.Auth do
 
   alias ZaZaar.Account
 
-  def fb_auth(%UAuth{uid: uid, info: info} = uauth) do
-    case Account.get_user(fb_id: uid) do
-      nil ->
-        %{fb_id: uid, name: info.name, email: info.email, image_url: info.image}
-        |> Account.set_user()
-
-      user ->
-        {:ok, user}
-    end
+  def fb_auth(%UAuth{uid: uid} = uauth) do
+    Account.get_user(fb_id: uid)
+    |> post_fb_set_user(uauth)
   end
 
   def fb_auth(_), do: {:error, :ueberauth_struct_not_used}
+
+  defp post_fb_set_user(nil, %UAuth{uid: uid, info: info, credentials: creds}) do
+    %{
+      fb_id: uid,
+      name: info.name,
+      email: info.email,
+      image_url: info.image,
+      fb_access_token: creds.token
+    }
+    |> Account.set_user()
+  end
+
+  defp post_fb_set_user(user, %UAuth{credentials: creds}) do
+    Account.set_user(user, %{fb_access_token: creds.token})
+  end
 end
