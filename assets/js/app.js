16b63c88f3b040920ab6aba987c52d6d1a0aeb91
Mickey <poverwhelming@gmail.com>
2018-12-18 16:42:25 +0800

    imported js loading setup from previous project

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,21 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+import loadView from './views/loader';
+
+function handleDOMContentLoaded() {
+  const viewName = document.getElementsByTagName('body')[0].dataset.jsViewPath;
+  const view = loadView(viewName);
+
+  view.mount();
+
+  window.currentView = view;
+}
+
+function handleDocumentUnload() {
+  window.currentView.unmount();
+}
+
+window.addEventListener('DOMContentLoaded', handleDOMContentLoaded, false);
+window.addEventListener('unload', handleDocumentUnload, false);
