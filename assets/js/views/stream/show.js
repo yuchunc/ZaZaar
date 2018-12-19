ab62aea586bcb1d1bda1e52627a06e51e44926ac
Mickey <poverwhelming@gmail.com>
2018-12-19 16:53:15 +0800

    finished edit action

--- assets/js/views/stream/show.js
+++ assets/js/views/stream/show.js
@@ -2,14 +2,37 @@ import main from '../main';
 
 let merchSnapshotModal = document.getElementById("merch-snapshot-modal");
 
-const tbodyOnclick = (e) => {
+const tbodyOnclickHandler = (e) => {
   let elem = e.target
 
   if(elem.classList.contains("merch-snapshot")) {
     event.preventDefault();
     merchSnapshotModal.getElementsByTagName("img")[0].src = elem.src;
     merchSnapshotModal.classList.add("is-active");
+  } else if(elem.classList.contains('edit-merch')) {
+    event.preventDefault();
+    let editables = elem.closest("tr").getElementsByClassName("editable");
+    for(let td of editables) {
+      if(td.classList.contains("merch-actions")) {
+        console.log("td", td);
+        let editBtn = td.getElementsByClassName("edit-merch")[0];
+        let saveBtn = el(`<a class="button is-success is-outlined">儲存</a>`);
+
+        editBtn.replaceWith(saveBtn);
+      } else {
+        replaceInnerWithInput(td);
+      }
+    };
   };
+};
+
+const replaceInnerWithInput = (td) => {
+  td.innerHTML = `<input class="input" type="text" value="${td.innerHTML}">`;
+};
+
+const el = ( domstring ) => {
+    const html = new DOMParser().parseFromString( domstring , 'text/html');
+    return html.body.firstChild;
 };
 
 const closeModal = () => {
@@ -20,7 +43,7 @@ const closeModal = () => {
 const mount = () => {
   let tbody = document.getElementById("items-table-body");
 
-  tbody.addEventListener('click', tbodyOnclick);
+  tbody.addEventListener('click', tbodyOnclickHandler);
 
   merchSnapshotModal.onclick = closeModal;
 
