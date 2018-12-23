56e3a27679fb10dbe884a9006258eb05f28ac73f
Mickey <poverwhelming@gmail.com>
2018-12-23 19:02:12 +0800

    fixed issue when not authentiated

--- lib/zazaar/auth/error_handler.ex
+++ lib/zazaar/auth/error_handler.ex
@@ -1,9 +1,9 @@
 defmodule ZaZaar.Auth.ErrorHandler do
   use ZaZaarWeb, :controller
 
-  def auth_error(conn, {:unauthenticated, reason}, _opts) do
+  def auth_error(conn, {type, _reason}, _opts) do
     conn
-    |> put_flash(:danger, reason)
+    |> put_flash(:danger, to_string(type))
     |> redirect(to: "/")
   end
 end
