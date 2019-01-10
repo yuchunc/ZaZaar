d32f01d02f334e3bfae6c4a2831d294773e3d032
Mickey <poverwhelming@gmail.com>
2019-01-10 15:42:04 +0800

    set webhook validation

--- lib/zazaar_web/controllers/webhook/facebook_controller.ex
+++ lib/zazaar_web/controllers/webhook/facebook_controller.ex
@@ -1,10 +1,15 @@
 defmodule ZaZaarWeb.Webhook.FacebookController do
   use ZaZaarWeb, :controller
 
-  def show(conn, %{"id" => "facebook"} = params) do
-    %{"hub.challenge" => challenge} = params
+  @webhook_secret Application.get_env(:facebook, :webhook_secret)
 
-    send_resp(conn, :ok, challenge)
+  def show(conn, params) do
+    if params["hub.verify_token"] == @webhook_secret do
+      %{"hub.challenge" => challenge} = params
+      send_resp(conn, :ok, challenge)
+    else
+      send_resp(conn, :unauthorized, "")
+    end
   end
 
   def create(conn, params) do
