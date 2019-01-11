06cdb28bf622b818ba5c2eaca0c8efe705e6401c
Mickey <poverwhelming@gmail.com>
2019-01-11 16:46:20 +0800

    finished PageChannel.join test, ffs

--- lib/zazaar_web/channels/page_channel.ex
+++ lib/zazaar_web/channels/page_channel.ex
@@ -2,17 +2,24 @@ defmodule ZaZaarWeb.PageChannel do
   use ZaZaarWeb, :channel
 
   def join("page:" <> fb_page_id, %{page_token: page_token}, socket0) do
-    {:ok, socket1} =
-      Guardian.Phoenix.Socket.authenticate(socket0, Auth.Guardian, page_token, %{}, key: :page)
-
-    page = current_page(socket1)
-
-    # NOTE possibily verify if user has the proper permission to manage the page
-    # check Page module for more detail on page permission
-    if page.fb_page_id == fb_page_id do
+    with {:ok, socket1} <-
+           Guardian.Phoenix.Socket.authenticate(socket0, Auth.Guardian, page_token, %{},
+             key: :page
+           ),
+         # NOTE possibily verify if user has the proper permission to manage the page
+         # check Page module for more detail on page permission
+         page <- current_page(socket1),
+         true <- page.fb_page_id == fb_page_id do
+      send(self(), {:after_auth, page})
       {:ok, socket1}
     else
-      {:error, %{reason: "forbidden"}}
+      :error -> {:error, %{reason: "unauthorized"}}
+      false -> {:error, %{reason: "forbidden"}}
     end
+  end
+
+  def handle_info({:after_auth, page}, socket) do
+    Fb.start_subscribe(page)
+    {:noreply, socket}
   end
 end
--- test/zazaar_web/channels/page_channel_test.exs
+++ test/zazaar_web/channels/page_channel_test.exs
@@ -1,7 +1,13 @@
 defmodule ZaZaarWeb.PageChannelTest do
   use ZaZaarWeb.ChannelCase
 
+  import Mox
+
   alias ZaZaarWeb.PageChannel
+  alias ZaZaar.FbApiMock, as: ApiMock
+
+  setup :set_mox_global
+  setup :verify_on_exit!
 
   setup do
     user = insert(:user)
@@ -13,9 +19,15 @@ defmodule ZaZaarWeb.PageChannelTest do
   describe "join a channel" do
     test "only signed in user can join page channel", ctx do
       %{page: page, user: user} = ctx
+
+      expect(ApiMock, :publish, fn :subscribed_apps, _obj_id, _, _access_token ->
+        {:ok, %{"success" => true}}
+      end)
+
       {:ok, user_token, _} = ZaZaar.Auth.Guardian.encode_and_sign(user)
       {:ok, page_token, _} = ZaZaar.Auth.Guardian.encode_and_sign(page)
-      {:ok, socket} = connect(UserSocket, %{user_token: user_token})
+
+      assert {:ok, socket} = connect(UserSocket, %{user_token: user_token})
 
       assert {:ok, _reply, _socket} =
                join(socket, "page:" <> page.fb_page_id, %{page_token: page_token})
