1cced8dfe5e9d36d9df0dd8d96a74c999a32a91d
Mickey <poverwhelming@gmail.com>
2019-02-13 15:08:00 +0800

    organized files

--- lib/zazaar_web.ex
+++ lib/zazaar_web.ex
@@ -39,7 +39,7 @@ defmodule ZaZaarWeb do
 
       # Import convenience functions from controllers
       import Phoenix.Controller,
-        only: [get_flash: 1, get_flash: 2, view_module: 1, action_name: 1]
+        only: [get_flash: 1, get_flash: 2, view_module: 1, action_name: 1, controller_module: 1]
 
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
