371907adea8b29c73acdf5b05f7de426fa565bb2
Mickey <poverwhelming@gmail.com>
2019-02-17 16:36:08 +0800

    added embedded Item schema

---
+++ lib/zazaar/booking/item.ex
@@ -0,0 +1,19 @@
+defmodule ZaZaar.Booking.Item do
+  use ZaZaar, :schema
+
+  @type t :: %__MODULE__{
+          merchandise_id: String.t(),
+          title: String.t(),
+          price: integer,
+          status: OrderItemStatus.__enum_map__()
+        }
+
+  embedded_schema do
+    field :merchandise_id, :string, null: false
+    field :title, :string, null: false
+    field :price, :integer, null: false
+    field :status, OrderItemStatus
+
+    timestamps
+  end
+end
