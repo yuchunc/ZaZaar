20a48f16329111bff8af191e572644f7d2acbac9
Mickey <poverwhelming@gmail.com>
2019-02-21 17:37:26 +0800

    hooked up Order#Index with data

--- lib/zazaar_web/controllers/order_controller.ex
+++ lib/zazaar_web/controllers/order_controller.ex
@@ -2,7 +2,11 @@ defmodule ZaZaarWeb.OrderController do
   use ZaZaarWeb, :controller
 
   def index(conn, _) do
-    render(conn, "index.html")
+    page = current_page(conn)
+
+    orders = Booking.get_orders([page_id: page.fb_page_id], preload: :buyer)
+
+    render(conn, "index.html", orders: orders)
   end
 
   def show(conn, %{"id" => id}) do
