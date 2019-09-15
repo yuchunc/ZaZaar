1fab298a0fbe8da3ce28098d738eadab772528bd
Mickey <poverwhelming@gmail.com>
2019-09-15 15:40:21 +0800

    finished loading comment in to merch_modal

--- lib/zazaar_web/lives/streaming_live/merch_modal.ex
+++ lib/zazaar_web/lives/streaming_live/merch_modal.ex
@@ -26,28 +26,38 @@ defmodule ZaZaarWeb.StreamingLive.MerchModal do
   end
 
   def handle_info({:mounted, session}, socket) do
-    %{page_id: page_id} = session
-    page = Account.get_page(page_id)
+    %{page_id: page_id, fb_video_id: fb_video_id} = session
 
-    {:noreply, assign(socket, :access_token, page.access_token)}
+    assigns = %{
+      access_token: Account.get_page(page_id) |> Map.get(:access_token),
+      video_id: Transcript.get_video(fb_video_id) |> Map.get(:id)
+    }
+
+    {:noreply, assign(socket, assigns)}
   end
 
   def handle_info(%{action: :new_merch} = payload, socket) do
     %{object_id: object_id, fb_video_id: fb_video_id} = payload
-    %{assigns: %{access_token: access_token}} = socket
+    %{assigns: %{access_token: access_token, video_id: vid_id}} = socket
 
     today = Date.utc_today # NOTE this might need to take user tz
     comment = Transcript.get_comment(object_id)
     {:ok, %{"data" => thumbnails}} = Fb.video_thumbnails(fb_video_id, access_token)
-    [vid_count] = Transcript.get_videos([fb_video_id: fb_video_id], get_count: true, on_date: today)
+    [vid_count] = Transcript.get_videos([], get_count: true, on_date: today)
+    [merch_count] = case Transcript.get_merchandises([video_id: vid_id], get_count: true) do
+      [] -> [ 0 ]
+      result -> result
+    end
+    [price] = Regex.scan(~r/\d+/, comment.message)
 
     assigns = %{
       show_modal: true,
       has_snapshot: true,
-      snapshot_url: thumbnails,
+      snapshot_url: thumbnails |> List.last() |> Map.get("uri"),
       commenter_fb_name: comment.commenter_fb_name,
       message: comment.message,
-      title: "#{inspect(today)} #{vid_count}" #{merch_count}"
+      title: gettext("%{today} Stream #%{vid_count} Merchandise #%{merch_count}", today: today, vid_count: vid_count, merch_count: merch_count + 1),
+      price: price
     }
 
     {:noreply, assign(socket, assigns)}
