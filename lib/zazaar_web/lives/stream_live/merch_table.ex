5a755a8a70bc7f7c60bd5742a579b6413a1cd3d2
Mickey <poverwhelming@gmail.com>
2019-10-03 16:07:36 +0800

    finished merch_table on stream page

--- lib/zazaar_web/lives/stream_live/merch_table.ex
+++ lib/zazaar_web/lives/stream_live/merch_table.ex
@@ -2,7 +2,6 @@ defmodule ZaZaarWeb.StreamLive.MerchTable do
   use ZaZaarWeb, :live
 
   @default_assign %{
-    show_snapshot_modal: false,
     snapshot_modal_url: "",
     merchs: []
   }
@@ -17,6 +16,8 @@ defmodule ZaZaarWeb.StreamLive.MerchTable do
       |> Map.put(:video, video)
 
     send(self(), {:mounted, video.id})
+    Phoenix.PubSub.subscribe(ZaZaar.PubSub, "stream:#{video.id}")
+
     {:ok, assign(socket, assigns)}
   end
 
@@ -29,12 +30,82 @@ defmodule ZaZaarWeb.StreamLive.MerchTable do
     {:noreply, assign(socket, :merchs, merchs)}
   end
 
+  def handle_info(%{action: :save_merch} = payload, socket) do
+    %{merch_id: merch_id} = payload
+    %{merchs: merchs} = socket.assigns
+    merch = Transcript.get_merchandise(merch_id)
+    {:noreply, assign(socket, :merchs, [merch | merchs])}
+  end
+
+  def handle_info(_, socket), do: {:noreply, socket}
+
   def handle_event("toggle-snapshot-modal", %{"snapshot" => snapshot_url}, socket) do
     assigns = %{
-      show_snapshot_modal: !socket.assigns.show_snapshot_modal,
       snapshot_modal_url: snapshot_url
     }
 
     {:noreply, assign(socket, assigns)}
+  end
+
+  def handle_event("toggle-edit-merch", %{"merch-id" => merch_id}, socket) do
+    %{merchs: merchs0} = socket.assigns
+
+    merchs1 =
+      Enum.map(merchs0, fn
+        %{id: ^merch_id, editing: editing} = m -> Map.put(m, :editing, !editing)
+        %{id: ^merch_id} = m -> Map.put(m, :editing, true)
+        m -> m
+      end)
+
+    {:noreply, assign(socket, :merchs, merchs1)}
+  end
+
+  def handle_event("update-merch", %{"merch" => merch_params}, socket) do
+    %{merchs: merchs0} = socket.assigns
+    merch_id = merch_params["id"]
+
+    {:ok, merch} =
+      merch_id
+      |> Transcript.get_merchandise()
+      |> Transcript.save_merchandise(%{
+        price: merch_params["price"],
+        title: merch_params["title"]
+      })
+
+    merchs1 =
+      Enum.map(merchs0, fn
+        %{id: ^merch_id} -> merch
+        m -> m
+      end)
+
+    {:noreply, assign(socket, :merchs, merchs1)}
+  end
+
+  def handle_event("update-merch", _, socket), do: {:noreply, socket}
+
+  def handle_event("toggle-merch-valid", %{"merch-id" => merch_id}, socket) do
+    %{merchs: merchs0} = socket.assigns
+
+    {:ok, merch} = merch_id |> Transcript.get_merchandise() |> toggle_merch_validity
+
+    merchs1 =
+      Enum.map(merchs0, fn
+        %{id: ^merch_id} -> merch
+        m -> m
+      end)
+
+    {:noreply, assign(socket, :merchs, merchs1)}
+  end
+
+  defp toggle_merch_validity(merch) do
+    invalidated_at =
+      if !merch.invalidated_at do
+        NaiveDateTime.utc_now()
+      end
+
+    merch
+    |> Map.from_struct()
+    |> Map.put(:invalidated_at, invalidated_at)
+    |> Transcript.save_merchandise()
   end
 end
