6591e09ebac89b88ea04c5431a4de5d4e76028e9
Mickey Chen <hey.mickabo@gmail.com>
2020-03-26 01:12:33 +0800

    got project running again

--- lib/zazaar_web.ex
+++ lib/zazaar_web.ex
@@ -38,11 +38,10 @@ defmodule ZaZaarWeb do
         root: "lib/zazaar_web/templates",
         namespace: ZaZaarWeb
 
-      # Import convenience functions from controllers
       import Phoenix.Controller,
         only: [get_flash: 1, get_flash: 2, view_module: 1, action_name: 1, controller_module: 1]
 
-      import Phoenix.LiveView, only: [live_render: 2, live_render: 3, live_link: 1, live_link: 2]
+      import Phoenix.LiveView.Helpers, only: [live_render: 3]
 
       use Phoenix.HTML
 
