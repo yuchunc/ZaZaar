19174478ef2906bf23085f98d604f94f637f6d09
Mickey Chen <hey.mickabo@gmail.com>
2020-03-31 21:50:36 +0800

    fixed an alignment issue with streaming comment input

--- lib/zazaar_web/lives/streaming_live/comment_area.ex
+++ lib/zazaar_web/lives/streaming_live/comment_area.ex
@@ -1,11 +1,76 @@
 defmodule ZaZaarWeb.StreamingLive.CommentArea do
-  use Phoenix.LiveView, container: {:div, class: "tile is-parent is-4 streaming__comments"}
+  use ZaZaarWeb, :live
 
   alias ZaZaar.Account
   alias ZaZaar.Transcript
   alias ZaZaarWeb.StreamView
 
-  def render(assigns), do: StreamView.render("comment_area.html", assigns)
+  def render(assigns) do
+      ~L"""
+    <div class="tile is-child card">
+      <header class="card-header">
+        <p class="card-header-title">
+          <%= gettext "Live Comments" %>
+        </p>
+      </header>
+      <div class="card-content comments" id="streaming-comments-list" phx-hook="COMMENT_LIST">
+        <%= for comment <- @comments do %>
+          <%= comment_elem(comment: comment, fb_video_id: @fb_video_id) %>
+        <% end %>
+      </div>
+      <div class="card-footer has-background-light">
+        <div class="media">
+          <figure class="media-left image is-32x32 is-avatar">
+            <img class="is-rounded" src="<%= if @page.picture_url, do: @page.picture_url, else: "https://bulma.io/images/placeholders/30x30.png"%>">
+          </figure>
+          <form phx-submit="new_comment">
+            <div class="media-content">
+              <div class="field">
+                <div class="control">
+                  <input class="ui fluid input" id="comment-input" name="comment" value="<%= @textarea %>" autocomplete="off" />
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    """
+    # <textarea class="input" id="comment-input" name="comment" placeholder="<%= gettext("Comment Here...") %>"><%= @textarea %></textarea>
+  end
+
+  def comment_elem(assigns) do
+    assigns = Enum.into(assigns, %{})
+    ~L"""
+    <div class="media comment-panel">
+      <figure class="media-left image is-32x32 is-avatar">
+        <img class="is-rounded" src=<%= @comment.commenter_picture %>>
+      </figure>
+      <div class="media-content">
+        <div class="content comment has-background-light is-marginless">
+          <p>
+            <span class="is-username has-text-primary has-text-weight-semibold">
+              <%= @comment.commenter_fb_name %>
+            </span>
+            <%= @comment.message %>
+          </p>
+        </div>
+        <!-- 留言時間 + 手動新增 btn -->
+        <div class="level is-mobile">
+          <small class="has-text-grey-light">
+            15 小時
+          </small>
+          <button class="button is-small is-outlined has-text-grey is-hover-primary new-merch"
+                  phx-click="enable-merch-modal"
+                  phx-value-object-id="<%= @comment.object_id %>"
+          >
+            <%= gettext "New Merch" %>
+          </button>
+        </div>
+      </div>
+    </div>
+    """
+  end
 
   def mount(_, session, socket) do
     %{"video_id" => video_id, "page_id" => page_id} = session
